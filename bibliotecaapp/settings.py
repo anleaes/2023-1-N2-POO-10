20e0a1b7ac244cd4f2919549a43f34c442e500eb
Joao1909 <paulopereirajoao98@gmail.com>
2023-06-13 21:35:27 -0300

    colocando parametros das classes dentro do settings.py

--- bibliotecaapp/settings.py
+++ bibliotecaapp/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'emprestimos.apps.emprestimosConfig',
+    'editoras.apps.editorasConfig',
 ]
 
 MIDDLEWARE = [
