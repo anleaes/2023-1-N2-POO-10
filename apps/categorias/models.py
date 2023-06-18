d53c8570128b9231d6e763b15c655720812c3a61
edudmatos <edumatos96@outlook.com>
2023-06-18 11:43:05 -0300

    Criação models categorias

--- apps/categorias/models.py
+++ apps/categorias/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+#Criando a classe
+
+class Categoria(models.Model):
+    #adicionando seus atributos
+    nome = models.CharField('Nome', max_length=200)
+    descricao = models.TextField('Descricao')
+    
+    #Criando a classe meta
+        
+     class Meta:
+            verbose_name_plural = "Categorias"
+
+
+    def __str__(self):
+        return self.nome
