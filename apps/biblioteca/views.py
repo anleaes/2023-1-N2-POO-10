d88c3bd7f928e2f564a133b46512db143b128e8c
GuiDrakeen <gui.duel@gmail.com>
2023-06-18 18:43:50 -0300

    teste geral

--- apps/biblioteca/views.py
+++ apps/biblioteca/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from .models import Biblioteca
 from rest_framework import viewsets
 from .serializer import BibliotecaSerializer
 
 # Create your views here.
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class BibliotecaViewSet(viewsets.ModelViewSet):
     queryset = Biblioteca.objects.all()
     serializer_class = BibliotecaSerializer
--- apps/editoras/models.py
+++ apps/editoras/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Editora(models.Model):
     nome = models.CharField('Nome', max_length=50)
     endereco = models.CharField('Endereço', max_length=200)
-    livros_publicados = models.ManyToManyField('Livro', verbose_name='Livros Publicados')
+    #livros_publicados = models.ManyToManyField('Livro', verbose_name='Livros Publicados')
 
     class Meta:
         verbose_name = 'Editora'
