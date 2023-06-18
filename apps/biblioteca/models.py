59c68d51f0661febe984097f5849b4bd25062550
edudmatos <edumatos96@outlook.com>
2023-06-18 08:34:51 -0300

    Corrigindo indentação

--- apps/biblioteca/models.py
+++ apps/biblioteca/models.py
@@ -24,26 +24,26 @@ class Biblioteca(models.Model):
     
     def buscarLivroPorTitulo(self, titulo):
         livros_encontrados = []
-    for livro in self.livros.all():
-        if livro.titulo == titulo:
-            livros_encontrados.append(livro)
-    return livros_encontrados
+        for livro in self.livros.all():
+            if livro.titulo == titulo:
+                livros_encontrados.append(livro)
+        return livros_encontrados
 
     def buscarLivroPorAutor(self, autor):
         livros_encontrados = []
-    for livro in self.livros.all():
-        if livro.autor.nome == autor:
-            livros_encontrados.append(livro)
-    return livros_encontrados
+        for livro in self.livros.all():
+            if livro.autor.nome == autor:
+                livros_encontrados.append(livro)
+        return livros_encontrados
 
     #criando o método de mostrar disponibilidade
     
     def listarLivrosDisponiveis(self):
         livros_disponiveis = []
-    for livro in self.livros.all():
-        if not Emprestimo.objects.filter(livro=livro).exists():
-            livros_disponiveis.append(livro)
-    return livros_disponiveis
+        for livro in self.livros.all():
+            if not Emprestimo.objects.filter(livro=livro).exists():
+                livros_disponiveis.append(livro)
+        return livros_disponiveis
 
     #criando os métodos de registro de usuário, atrelado a classe 'Usuario'
     
