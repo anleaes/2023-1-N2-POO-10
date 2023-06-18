b84b54cf1b1b162856fd96a44b66d312738eb987
edudmatos <edumatos96@outlook.com>
2023-06-18 10:14:54 -0300

    Criação da views biblioteca

--- apps/biblioteca/views.py
+++ apps/biblioteca/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Biblioteca
+from rest_framework import viewsets
+from .serializer import BibliotecaSerializer
 
 # Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Biblioteca.objects.all()
+    serializer_class = BibliotecaSerializer
